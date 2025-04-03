e8aeeebfc7b9569a32a59c1c6d34eb6cff364e14
kirshe <kirshe@yandex-team.com>
2025-04-03 13:43:39 +0500

    Changed the module source to using absolute url.

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,6 @@
 module "ocr" {
-  source = "../terraform/"
-
+  source = "github.com/yandex-cloud-examples/yc-vision-ocr-recognizer/terraform"
+  
   folder_id         = var.folder_id
   cloud_id          = var.cloud_id
 }
